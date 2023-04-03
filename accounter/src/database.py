4a4c8e3d4f7cdd7780ae04bf33c4e25ec2ed3878
tatrik <tatrik9@gmail.com>
2023-04-03 12:02:52 +0200

    create first part of project

---
+++ accounter/src/database.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from src.settings import settings
+
+
+engine = create_engine(
+    settings.database_url,
+    connect_args={"check_same_thread": False},
+)
+
+
+Session = sessionmaker(
+    engine,
+    autocommit=False,
+    autoflush=False,
+)
+
