cb57a2bad23dfe1fa9914e565c5ec10b23fcb6a8
tatrik <tatrik9@gmail.com>
2023-04-05 21:03:37 +0200

    added authoricate

--- accounter/src/api/__init__.py
+++ accounter/src/api/__init__.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
+from  src.api.auth import router as auth_router
 from src.api.operations import router as operations_router
 
 
 router = APIRouter()
+router.include_router(auth_router)
 router.include_router(operations_router)
--- accounter/src/tables.py
+++ accounter/src/tables.py
@@ -10,7 +10,7 @@ class User(Base):
 
     id = sa.Column(sa.Integer, primary_key=True)
     email = sa.Column(sa.Text, unique=True)
-    name = sa.Column(sa.Text, unique=True)
+    username = sa.Column(sa.Text, unique=True)
     password_hash = sa.Column(sa.Text)
 
 
