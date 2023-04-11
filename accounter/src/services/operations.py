129a7e5eadb5f6ae9e5bee7b48de05410ae90a9a
tatrik <tatrik9@gmail.com>
2023-04-11 17:58:53 +0200

    added import and export from/to csv file

--- accounter/src/services/operations.py
+++ accounter/src/services/operations.py
@@ -40,6 +40,18 @@ class OperationsService:
     def get(self, user_id: int, operation_id: int) -> Type[Operation]:
         return self._get(user_id=user_id, operation_id=operation_id)
 
+    def create_many(self, user_id: int, operations_data: List[OperationCreate]) -> List[tables.Operation]:
+        operations = [
+            tables.Operation(
+                **operation_data.dict(),
+                user_id=user_id,
+            )
+            for operation_data in operations_data
+        ]
+        self.session.add_all(operations)
+        self.session.commit()
+        return operations
+
     def create(self, user_id: int, operation_data: OperationCreate) -> tables.Operation:
         operation = tables.Operation(
             **operation_data.dict(),
